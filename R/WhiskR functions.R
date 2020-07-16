4cc121fc734d97ad69004629a70d6f87d22a8ed6
Gary Truong <67947493+GaryTruong@users.noreply.github.com>
2020-07-16 11:43:54 +1000

    Updates

--- R/WhiskR functions.R
+++ R/WhiskR functions.R
@@ -1,12 +1,15 @@
 #' Calculate Segment Length
 #'
 #' This function allows you calculate where to cut to represent a specific time period.
-#' @param L Length of the cut whisker
-#' @param L.asym Asymptotic length
-#' @param Time Time period to be represented by the cut segment in days
-#' @param k Growth coefficient, calculate using the function k
-#' @keywords whisker segment length
+#' It uses the discrete step by step Von Bertanlanffy equation
+#' @param L numeric; length of the cut sample
+#' @param L.asym numeric; asymptotic length for the sample
+#' @param Time numeric; time period to be represented by the cut segment in days
+#' @param k numeric; growth coefficient, calculate using the function k
+#' @keywords whisker segment length, Von-Bertanlanffy
 #' @export
+#' @author
+#' @references Hall-Aspland, S. A., Rogers, T. L., & Canfield, R. B. (2005). Stable carbon and nitrogen isotope analysis reveals seasonal variation in the diet of leopard seals. Marine Ecology Progress Series, 305, 249-259.
 #' @return Where to cut along the whisker
 #' @examples
 #' LP1(L=100, L.asym=150,Time=28 k=0.0126)
@@ -19,6 +22,7 @@ LP1 <- function(L, L.asym, Time, k){
 #' Calculate Time Represented by Segment
 #'
 #' This function allows you calculate how much time is represented by a sampled section of whisker.
+#' It uses the discrete step by step Von Bertanlanffy equation
 #' @param L Length of the cut whisker
 #' @param L.asym Asymptotic length
 #' @param LP1 Length of the whisker minus the cut sampled segment
@@ -60,9 +64,10 @@ Intra.time <- function(L.int, L.asym, L, k){
   paste(time, "days", sep = " ")
 }
 
-
-
-
+#' Calculate Maximum Time represented by sample
+#'
+#' This function uses the simple Von Bertanlanffy equations to calculate the time represented by the entire sample
+#' @export
 MaxTime <- function(L , L.asym, k){
   Time = format((-log(1-L/L.asym))/(k), digits = 4, nsmall = 1)
   paste(Time, "days", sep = " ")
